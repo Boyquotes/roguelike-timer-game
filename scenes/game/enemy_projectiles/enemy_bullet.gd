54f4958243703b2a72454619026ef8042be65edd
ne_mene21 <73829851+nemene21@users.noreply.github.com>
2023-05-16 15:51:31 +0200

    enemy bullet spawn animation

--- scenes/game/enemy_projectiles/enemy_bullet.gd
+++ scenes/game/enemy_projectiles/enemy_bullet.gd
@@ -1,8 +1,12 @@
 extends Sprite2D
 
 var velocity : Vector2
+var rotation_vel := 180.0
+
 
 func _process(delta):
 	$glow.scale = 0.16 * (abs(sin(Global.time * 2)) * 0.2 + 0.8) * Vector2(1, 1)
 	
 	global_position += velocity * delta
+	
+	rotation_degrees += rotation_vel * delta
